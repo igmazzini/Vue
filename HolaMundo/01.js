506ca8d30946e60b5fd35fb6f2b3fbe8cc0ae9d0
Nacho <igmazzini2013@gmail.com>
2020-04-22 18:51:17 -0300

    Componentes

--- HolaMundo/01.js
+++ HolaMundo/01.js
@@ -1,8 +1,26 @@
+Vue.component('componente1', {
+    template:
+     `
+    <div>
+        <h1>
+        {{saludo}}
+        </h1>
+        <h3>caccsac</h3>
+    </div>
+    `,
+    data() {
+        return {
+            saludo: 'Hola mundo desde componente'
+        }
+    }
+
+});
+
 const app = new Vue({
     el: "#app",
     data: {
         titulo: "Hola Mundo Vue",
-        frutaNueva:{name:'',amount:0},
+        frutaNueva: { name: '', amount: 0 },
         frutas: [
             { name: 'Pera', amount: 10 },
             { name: 'Manzana', amount: 0 },
@@ -10,14 +28,15 @@ const app = new Vue({
             { name: 'Mango', amount: 12 },
             { name: 'Papaya', amount: 50 }
         ],
-        total:0,
-        fondo:'bg-dark',
-        textColor:'text-white'
+        total: 0,
+        fondo: 'bg-dark',
+        textColor: 'text-white',
+
     },
-    methods:{
-        addNuevaFruta(){
-            if(this.frutaNueva.name != ''){
-                var fruta = {name:'',amount:0};
+    methods: {
+        addNuevaFruta() {
+            if (this.frutaNueva.name != '') {
+                var fruta = { name: '', amount: 0 };
                 fruta.name = this.frutaNueva.name;
                 fruta.amount = this.frutaNueva.amount;
                 this.frutas.push(fruta);
@@ -26,54 +45,96 @@ const app = new Vue({
                 this.updateLocalStorage();
             }
             this.fondo = 'bg-success';
-            
+
         },
 
-        removeFruta(index){
-            if(this.frutas[index].amount > 0){
-                this.frutas[index].amount --;
+        removeFruta(index) {
+            if (this.frutas[index].amount > 0) {
+                this.frutas[index].amount--;
                 this.updateLocalStorage();
-               
+
             }
-            
+
         },
-        addFruta(index){
+        addFruta(index) {
             this.frutas[index].amount++;
             this.updateLocalStorage();
-           
-            
+
+
         },
-        eliminarFruta(index){
-            this.frutas.splice(index,1);
+        eliminarFruta(index) {
+            this.frutas.splice(index, 1);
             this.updateLocalStorage();
         },
-        updateLocalStorage(){
-            localStorage.setItem('frutasLS',JSON.stringify(this.frutas));         
+        updateLocalStorage() {
+            localStorage.setItem('frutasLS', JSON.stringify(this.frutas));
         }
 
 
-    
+
 
     },
-    created: function(){
+    beforeCreate: function () {
+        console.log('BEFORE CREATE');
+    },
+    created: function () {
+        console.log('CREATED');
         var datosLS = JSON.parse(localStorage.getItem('frutasLS'));
-       
-        if(datosLS === null){           
-           this.updateLocalStorage();
-        }else{           
+
+        if (datosLS === null) {
+            this.updateLocalStorage();
+        } else {
             this.frutas = datosLS;
-            
+
         }
     },
-    computed:{
-        sumarFrutas(){
+    beforeMount: function () {
+        console.log('BEFORE MOUNT');
+    },
+    mounted: function () {
+        console.log('MOUNTED');
+    },
+    beforeUpdate: function () {
+       // console.log('BEFORE UPDATE');
+    },
+    updated: function () {
+       // console.log('UPDATED');
+    },
+    beforeDestroy: function () {
+        console.log('BEFORE DESTROY');
+    },
+    destroyed: function () {
+        console.log('DESTROYED');
+    },
+    computed: {
+        sumarFrutas() {
             this.total = 0;
             for (let i = 0; i < this.frutas.length; i++) {
                 this.total += this.frutas[i].amount;
-                
+
             }
 
             return this.total;
+        },
+
+        percent() {
+            var porcentaje = (100 * this.total) / 1000;
+            if (porcentaje > 100) {
+                porcentaje = 100;
+            }
+            return porcentaje;
+
+        },
+        barColor() {
+            var color = '';
+            if (this.percent <= 50) {
+                color = 'bg-success';
+            } else if (this.percent > 50 && this.percent < 80) {
+                color = 'bg-warning'
+            } else if (this.percent > 80) {
+                color = 'bg-danger';
+            }
+            return color;
         }
     }
 }); 
